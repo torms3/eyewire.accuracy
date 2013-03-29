d96e21a4f302a4a9f06dc91ec8c7c4e580854903
torms3 <kisuklee@mit.edu>
2013-03-29 06:55:27 -0400

    additional DB_tasks

---
+++ DB_tasks/DB_extract_cell_IDs.m
@@ -0,0 +1,37 @@
+function [cell_IDs] = DB_extract_cell_IDs( period )
+
+%% Argument validation
+%
+if( ~exist('period','var') )
+    period.since = '';
+    period.until = '';
+end
+
+
+%% MySQL
+%
+%	Extract unique cell IDs from EyeWire DB
+%
+
+% MySQL open
+mysql('open','127.0.0.1:13306','omnidev','we8pizza');
+mysql('use omniweb');
+
+% MySQL query 1
+query_str = ['SELECT DISTINCT(tasks.cell) '...
+         	 'FROM validations ' ...
+         	 'INNER JOIN tasks ON validations.task_id=tasks.id ' ...
+         	 % 'WHERE validations.status=0 and tasks.status=0 ' ...
+         	 % 'and user_id!=1 ' ...
+        	];
+
+% get WHERE clause
+[where_clause] = DB_get_where_clause( 0, period, 0, 0 );
+
+query_str = [query_str where_clause 'ORDER BY tasks.cell '];
+[cell_IDs] = mysql( query_str );
+
+% MySQL close
+mysql('close');
+
+end
