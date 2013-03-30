2caf5ea54130ac07198fae251570bea6eaae8a31
torms3 <kisuklee@mit.edu>
2013-03-30 03:08:02 -0400

    directory structure refactoring

--- DB_tasks/helper/add_cell_clause.m
+++
@@ -1,8 +0,0 @@
-function [query_str] = add_cell_clause( query_str, cell_ID )
-
-if( cell_ID ~= 0 )
-	cell_clause = sprintf('and tasks.cell=%d ',cell_ID);
-	query_str = [query_str cell_clause];
-end
-
-end
