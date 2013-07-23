5b053d315335b95a274d26903363c5b603c8b66c
torms3 <kisuklee@mit.edu>
2013-07-22 22:54:26 -0400

    where cluase

--- bin/DB_tasks/helper/get_where_clause.m
+++ bin/DB_tasks/helper/get_where_clause.m
@@ -7,7 +7,7 @@ v_status_str = get_condition_str( 'validations.status', v_status );
 where_clause = ['WHERE ' t_status_str 'AND ' v_status_str];
 
 % user_id 1 is reserved for reaping
-% where_clause = [where_clause 'AND (validations.user_id!=1) '];
+where_clause = [where_clause 'AND (validations.user_id!=1) '];
 
 % add cell clause, if any
 [where_clause] = add_cell_clause( where_clause, cell_IDs );
