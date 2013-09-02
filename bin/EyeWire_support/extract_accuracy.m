e2a0dbc94c2b8546af5c8d6dc0d5a84ba402c2d1
= <kisuklee@mit.edu>
2013-09-02 17:07:06 -0400

    push

--- bin/EyeWire_support/extract_accuracy.m
+++ bin/EyeWire_support/extract_accuracy.m
@@ -15,7 +15,7 @@ function [STAT] = extract_accuracy( period, celltype )
 	%% Cell IDs
 	%
 	[cellIDs] = DB_extract_cell_IDs( period, t_status );
-	if( strcmp(celltype,'SAC') )
+	if( strcmp(celltype,'sac') )
 		[MAP_celltype] = DB_create_MAP_celltype();
 		keys = cell2mat(MAP_celltype.keys);
 		vals = MAP_celltype.values;
